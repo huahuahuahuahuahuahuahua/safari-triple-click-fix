7b1848e358b647b200d9720496474a9be9c9550a
huahuahuahuahuahuahuahua <assans82@hotmail.com>
2026-09-17 15:27:37 +0800

    Initial Commit

---
+++ triple-click-fix/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  triple-click-fix
+//
+//  Created by  Tan Bangcheng on 2026/9/17.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
